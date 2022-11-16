4b56fe215140580395384d443c07dd6249e2d94f
Michael Tejada <76642717+qjmttejada@users.noreply.github.com>
2022-11-16 17:43:50 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This entrypoint file to be used in development. Start by reading README.md
+from probability_calculator import Hat
+from probability_calculator import experiment
+from unittest import main
+
+hat = Hat(blue=4, red=2, green=6)
+probability = experiment(
+    hat=hat,
+    expected_balls={"blue": 2,
+                    "red": 1},
+    num_balls_drawn=4,
+    num_experiments=3000)
+print("Probability:", probability)
+
+# Run unit tests automatically
+main(module='test_module', exit=False)

25c54077f18528ac94a6ae83633462543537c5f9
Michael Tejada <76642717+qjmttejada@users.noreply.github.com>
2022-11-17 23:19:50 +0800

    probability_calculator.py

--- probability_calculator.py
+++ probability_calculator.py
@@ -1,53 +1,23 @@
 import copy
 import random
+# Consider using the modules imported above.
 
 
 class Hat:
     def __init__(self, **kwargs):
+        self.contents = [k for k, v in kwargs.items() for _ in range(v)]
 
-        self.contents = []
-        for k, v in kwargs.items():
-            for i in range(v):
-                self.contents.append(k)
-
-        self.__initial_contents = copy.copy(self.contents)
-
-    def reset(self):
-        self.contents = copy.copy(self.__initial_contents)
-
-    def draw(self, count):
-        # Draw the specified count of items from the hat, return all items if the count is > number of items
-        try:
-            drawn = random.sample(self.contents, count)
-        except ValueError:
-            drawn = copy.copy(self.contents)
-
-        # Remove the items drawn from the hat
-        for item in drawn:
-            self.contents.remove(item)
-
-        # Refill the hat if empty
-        if len(self.contents) == 0:
-            self.contents = copy.copy(self.__initial_contents)
-
-        return drawn
+    def draw(self, n):
+        n = min(n, len(self.contents))
+        return [self.contents.pop(random.randrange(len(self.contents))) for _ in range(n)]
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    expected_count = 0
-    for i in range(num_experiments):
-        expected_balls_working_copy = copy.copy(expected_balls)
-        hat.reset()
-        returned_balls = hat.draw(num_balls_drawn)
-        for ball_color, ball_count in expected_balls.items():
-            for _ in range(ball_count):
-                if ball_color in returned_balls:
-                    returned_balls.remove(ball_color)
-                    expected_balls_working_copy[ball_color] -= 1
+    m = 0
+    for _ in range(num_experiments):
+        another_hat = copy.deepcopy(hat)
+        balls_drawn = another_hat.draw(num_balls_drawn)
+        balls_req = sum([1 for k, v in expected_balls.items() if balls_drawn.count(k) >= v])
+        m += 1 if balls_req == len(expected_balls) else 0
 
-        if sum(v for v in expected_balls_working_copy.values()) == 0:
-            expected_count += 1
-
-    probability = expected_count / num_experiments
-
-    return 
+    return m / num_experiments
